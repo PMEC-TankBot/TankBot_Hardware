2034373ad7364735e0e52177e92385869c08d046
MarceloJordao01 <marcelo.jordao@discente.ufg.br>
2025-05-28 14:02:49 -0300

    Test for microros

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,45 @@
-#include <Arduino.h>
+#include <micro_ros_arduino.h>
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <geometry_msgs/msg/twist.h>
 
-// put function declarations here:
-int myFunction(int, int);
+rcl_node_t node;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rclc_executor_t executor;
+
+rcl_subscription_t twist_sub;
+geometry_msgs__msg__Twist twist_msg;
+
+void twistCallback(const void *msgin) {
+  const geometry_msgs__msg__Twist *msg = (const geometry_msgs__msg__Twist *)msgin;
+  Serial.print("Recebido linear.x: ");
+  Serial.println(msg->linear.x);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  set_microros_serial_transports(Serial);
+
+  delay(2000); // tempo para estabilizar a serial
+
+  allocator = rcl_get_default_allocator();
+  rclc_support_init(&support, 0, NULL, &allocator);
+  rclc_node_init_default(&node, "microros_esp32_node", "", &support);
+
+  rclc_subscription_init_default(
+    &twist_sub,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
+    "cmd_vel"
+  );
+
+  rclc_executor_init(&executor, &support.context, 1, &allocator);
+  rclc_executor_add_subscription(&executor, &twist_sub, &twist_msg, &twistCallback, ON_NEW_DATA);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+  delay(10);
 }
